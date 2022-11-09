2ae5aa00b200461f4fad654c28ac33a145924ec6
FynIk <ash.glc@gmail.com>
2022-11-09 14:58:40 +0600

    clock refining

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,16 +2,20 @@ import { useState, useEffect } from "react";
 
 const Clock = () => {
   const [date, setDate] = useState(new Date());
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
+  
   const refreshClock = () => setDate(new Date());
-
+  
   useEffect(() => {
     const timer = setInterval(refreshClock, 1000);
     return () => clearInterval(timer);
   }, []);
+  
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-  return <p className="clock">Сегодня {date.toLocaleDateString("ru-RU", options)}, {date.toLocaleTimeString("ru-RU")}</p>
+  const dateString = date.toLocaleDateString("ru-RU", options);
+  const capitalizedDateString = dateString.charAt(0).toUpperCase() + dateString.slice(1);
+
+  return <p className="clock">{capitalizedDateString}, {date.toLocaleTimeString("ru-RU")}</p>
 }
 
 export default Clock;
